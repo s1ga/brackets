ef1abdf88753266e86b3b2eac59313717bb20d2d
s1ga <s.v.harlanov@gmail.com>
2021-01-20 19:34:36 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i <= bracketsConfig.length; i++) {
+    bracketsConfig.forEach(value => {
+        const brackets = value[0] + value[1]
+        while (str.indexOf(brackets) >= 0) {
+          const index = str.indexOf(brackets)
+          str = str.slice(0, index) + str.slice(index + 2)
+        }
+    })
+  }
+  return str ? false : true
 }
